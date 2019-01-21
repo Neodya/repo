fe636e6044c5ccbc62d3a91ec77a76cb2e384657
IssaC <iclement.neodya@gmail.com>
2019-01-21 10:03:30 +0100

    fichier.html remplacé par un fichier.php dans cette branche

---
+++ fichier1.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Page d'accueil</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <h1> WikiFlute </h1>
+  <em> Bienvenue sur Wiki flute, parce que... </em>
+  <p> ... Flute, y a rien :) ! lol <br/>
+  	  Ah bah non regardez il ya un formulaire en bas ! 
+  </p>
+  <form method="POST" action="formulaire.php">
+  	<p>
+  		<label for="pseudo">Votre pseudo</label><input type="text" name="pseudo" id="pseudo"><br/>
+
+  		<label for="pass">Votre mot de passe</label> : <input type="password" name="pass" id="pass"><br/>
+
+  		<input type="submit" value="Valider">
+  	</p>
+  	
+  </form>
+  
+
+
+
+</body>
+</html>
